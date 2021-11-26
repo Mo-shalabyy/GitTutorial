4ce4985f01c8e4ba0329b8e66937d1768c028e66
shalaby <muhamed.shalaby.ms@gmail.com>
2021-11-26 15:05:39 +0200

    first

---
+++ src/test/java/qaclickacademy/Mavenjava/appiumtest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class appiumtest {
+	@Test
+	public void nativeappandroid()
+{
+		System.out.println("nativeapp");
+}
+
+@Test
+public void iosapp() 
+{
+	
+	System.out.println("IOSap");
+}
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/seleniumtest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class seleniumtest {
+
+	
+	
+	@Test
+		public void browser()
+		{
+		System.out.println("browser");
+	}
+	
+	@Test
+	public void elements() {
+		System.out.println("elements");
+	}
+	
+}
